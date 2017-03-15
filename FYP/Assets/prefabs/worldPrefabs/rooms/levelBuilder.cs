8962f94a0b762469e70c37d91e1d4e30251c79fe
peter flanagan <peter flanagan>
2017-03-15 21:39:48 +0000

    Create prefabs for procedural generation

---
+++ FYP/Assets/prefabs/worldPrefabs/rooms/levelBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelBuilder : MonoBehaviour {
+
+    public GameObject[] rooms;
+    public GameObject[] corridors;
+
+    public GameObject startRoom;
+
+    private List<GameObject> curRooms;
+
+    private List<GameObject> tempRooms;
+
+
+	// Use this for initialization
+	void Start () {
+        curRooms = new List<GameObject>();
+        tempRooms = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void restartGen()
+    {
+        for(int i = 0; i < tempRooms.Count; i++)
+        {
+            Object.Destroy(tempRooms[i]);
+        }
+        tempRooms.Clear();
+    }
+}
---
+++ FYP/Assets/prefabs/worldPrefabs/rooms/roomController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roomController : MonoBehaviour {
+
+
+    public GameObject inDoor;
+    public GameObject outDoor;
+
+    public GameObject[] waypoints;
+
+    private GameObject levelBuilder;
+	// Use this for initialization
+	void Start () {
+        levelBuilder = GameObject.FindGameObjectWithTag("levelBuilder");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == 8)
+        {
+            levelBuilder.GetComponent<levelBuilder>().restartGen();
+        }
+    }
+}
